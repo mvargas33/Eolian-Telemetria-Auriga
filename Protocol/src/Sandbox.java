b88d4b972394e4c312820a7781f59e69a60b8ced
Max <mvargas33@protonmail.com>
2019-11-08 16:17:16 -0300

    Eliminadas cosas innecesarias en SandBox

--- Protocol/src/Sandbox.java
+++ Protocol/src/Sandbox.java
@@ -5,45 +5,27 @@ public class Sandbox {
     public static void main(String args[]) {
         // 0000  0001  0010  0011  0100  0101  0110  0111  1000  1001  1010  1011  1100  1101  1110  1111
         // 0     1     2     3     4     5     6     7     8     9     A     B     C     D     E     F
-        byte a = (byte) 0b00001001;
-        System.out.println(a);
-        byte b = (byte) (a &  0b00001000);
-        System.out.println(b);
-        /*
+
         BlockingQueue<Integer> sharedQueue = new LinkedBlockingQueue<>();
 
         ExecutorService pes = Executors.newFixedThreadPool(2);
-        ExecutorService ces = Executors.newFixedThreadPool(100);
+        ExecutorService ces = Executors.newFixedThreadPool(2);
 
         pes.submit(new Producer(sharedQueue, 1));
         pes.submit(new Producer(sharedQueue, 2));
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 2; i++){
             ces.submit(new Consumer(sharedQueue, i));
         }
 
-
         pes.shutdown();
         ces.shutdown();
-        */
+
         //int[] a = {1,2,3,4,5};
         //System.out.println(ts(a));
         //calc(a);
         //System.out.println(ts(a));
     }
 
-    public static void calc(int[] a){
-        a[0] = 1222;
-        return;
-    }
-
-    public static String ts(int[] a){
-        StringBuilder sb = new StringBuilder();
-        for (int aa: a
-             ) {
-            sb.append(String.valueOf(aa) + " ");
-        }
-        return sb.toString();
-    }
 }
 
 /* Different producers produces a stream of integers continuously to a shared queue, 
@@ -65,7 +47,7 @@ class Producer implements Runnable {
                 int number = random.nextInt(1000);
                 System.out.println("Produced:" + number + ":by thread:"+ threadNo);
                 sharedQueue.put(number);
-                Thread.sleep(200);
+                Thread.sleep(1000);
             } catch (Exception err) {
                 err.printStackTrace();
             }
