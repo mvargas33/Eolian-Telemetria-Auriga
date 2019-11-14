ebc7434ef25f80434d540f4be3dd4cf8377c22c5
Max <mvargas33@protonmail.com>
2019-11-14 18:44:05 -0300

    Implementada comunicación base JavaApp->ServidorNodeJS. Se implementa un HashMap en NodeJS como variable global de arreglo de componentes y sus arrays de valores. El JSON enviado envía dos llaves: componente, valores que son descifradas en el servidor

--- JS/server/index.js
+++ JS/server/index.js
@@ -2,16 +2,29 @@
 
 const express = require('express') // Para montar servidor
 const bodyParser = require('body-parser') // Para parsear solicitudes POST y otras
+const HashMap = require('hashmap')
 
 const app = express() // Servidor
 const port = process.env.PORT || 3000 // Puerto del servidor
 
-app.use(bodyParser.urlencoded({extended: false })) // Estas dos líneas son estándar
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true })) // Estas dos líneas son estándar
+app.use(bodyParser.json()) // Usar formato JSON para POST
 
-app.get('/hola/:name', (req, res) => { // /hola/:name envía parametro por GET y es accesible con ${req.params.name}
-    res.send({ message: `Hola ${req.params.name}`})
-}) // Ruta a la que estucha este bicho con peticion GET. Lambda recibe request y envía response
+// Variable global con todos los pares (componenteID, valores[])
+var map = new HashMap()
+map.set("BMS", [])
+map.set("MPPT1", [])
+
+
+app.post('/update', (req, res) => { // Listener a la ruta /update
+    //console.log(req.body) // DEBUG, Parse de los datos
+    var componente = req.body.componente // Tomamos el nombre del componente
+    var valores = JSON.parse(req.body.valores) // Transformamos el String de valors a Array
+    map.set(componente, valores) // Actualizamos variable global
+    console.log(map.get(componente)) // Imprimimos en consola para ver si se actualiza
+    res.status(200).send({message: 'El componente se ha actualizado'}) // Cósigo de retorno para App
+
+})
 
 app.listen(port, () => { // Pasamos una lambda que solo hace log
     console.log(`Servidor corriendo en http://localhost:${port}`) // Print inicial del servidor
--- Protocol/src/LocalSystems/ServerAdmin/ServerAdmin.java
+++ Protocol/src/LocalSystems/ServerAdmin/ServerAdmin.java
@@ -1,8 +1,52 @@
 package LocalSystems.ServerAdmin;
 
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.util.Arrays;
+
+
 /*
     Se encarga de enviar la información de los componentes al servidor web para su visualización.
     Esta atento a los eventos (heads-up) de los componentes para enviar información actualizada.
  */
 public class ServerAdmin {
+
+    public static void main(String[] args) {
+        String componentID = "BMS";
+        double[] valores = {1.23, 2.33, 3, 4, 52.33, 6, 7.2, 8, 9.13};
+
+        JSONObject json = new JSONObject();
+        json.put("componente", componentID);
+        json.put("valores", Arrays.toString(valores));
+
+        HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead
+
+        try {
+
+            HttpPost request = new HttpPost("http://localhost:3000/update");
+            StringEntity params =new StringEntity(json.toString());
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            HttpResponse response = httpClient.execute(request);
+            System.out.print(response.toString());
+
+            //handle response here...
+
+        }catch (Exception ex) {
+
+            //handle exception here
+
+        } finally {
+            //Deprecated
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
 }
