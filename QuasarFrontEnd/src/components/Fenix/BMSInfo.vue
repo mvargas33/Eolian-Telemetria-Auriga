dfe55966fe18ec36345df6be323107dd5cd131fc
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-04-01 19:25:45 -0300

    BMSInfo ready

--- QuasarFrontEnd/src/components/Fenix/BMSInfo.vue
+++ QuasarFrontEnd/src/components/Fenix/BMSInfo.vue
@@ -1,7 +1,54 @@
 <template>
-    <div class="row justify-center full-height full-width text-center">
-        BMS Info
+  <div>
+    <div class="allCenter">
+        <!-- BMS -->
     </div>
+    <div class="row">
+
+      <!-- 1 Etiquetas  -->
+      <div class="col">
+          <div class="text-left etiqueta texto-bold">
+              Pack Abs I
+          </div>
+          <div class="text-left etiqueta texto-bold">
+              Max Pack V
+          </div>
+      </div>
+      <!-- 2 Valores  -->
+      <div class="col">
+          <div class="text-center">
+              {{ bms[4] }} <!-- Pack Abs I -->
+          </div>
+          <div class="text-center">
+              {{ bms[13] }} | {{ bms[5] }} <!-- Max_Volt_ID | Max_Volt -->
+          </div>
+      </div>
+      <!-- 3 Current -->
+      <div class="col">
+          <div class="allCenter numero">
+              {{ bms[1] }} <!-- Current -->
+          </div>
+      </div>
+      <!-- 4 Etiqueta  -->
+      <div class="col">
+          <div class="text-left etiqueta texto-bold">
+              Pack Open V
+          </div>
+          <div class="text-left etiqueta texto-bold">
+              Min Pack V
+          </div>
+      </div>
+      <!-- 5 Valores  -->
+      <div class="col">
+          <div class="text-center">
+              {{ bms[3] }} <!-- Pack Open Voltage -->
+          </div>
+          <div class="text-center">
+              {{ bms[14] }} | {{ bms[6] }} <!-- Min_Volt_ID | Min_Volt -->
+          </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,7 +57,7 @@ import { mapState } from 'vuex'
 export default {
   name: 'BMSInfo',
   computed: {
-    ...mapState('fenix', ['mainData'])
+    ...mapState('fenix', ['bms'])
   }
 }
 </script>
--- QuasarFrontEnd/src/store/module-fenix/state.js
+++ QuasarFrontEnd/src/store/module-fenix/state.js
@@ -3,7 +3,7 @@ export default function () {
     mainData: [56, 3.4, 1.024, 89, 4.048, 3.876, 34.5, 27.9], // 8 [velocidad, potencia, mppt_in, soc, max_volt, min_volt, max_temp, min_temp]
     kelly_der: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], // 14 [ia, ib, ic, va, vb, vc, rpm, err_code, pwm, emr, motor_temp, kelly_temp, throttle, reverse]
     kelly_izq: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], // 14 [ia, ib, ic, va, vb, vc, rpm, err_code, pwm, emr, motor_temp, kelly_temp, throttle, reverse]
-    bms: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 15 [SOC, current, inst_volt, open_volt, abs_current, max_volt, min_volt, max_temp, max_temp_id, min_temp, min_temp_id, avg_temp, internal_temp, max_volt_id, min_volt_id]
+    bms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], // 15 [SOC, current, inst_volt, open_volt, abs_current, max_volt, min_volt, max_temp, max_temp_id, min_temp, min_temp_id, avg_temp, internal_temp, max_volt_id, min_volt_id]
     bms_temp: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 60
     bms_volt: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 30
     mppt1: [0, 0, 0, 0, 0, 0, 0, 0], // 8 [uin, iin, uout, blvr, ovt, noc, undv, temp]
