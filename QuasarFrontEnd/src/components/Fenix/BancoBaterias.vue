967aaa52131991df907a62cbc6f878faf2a658c4
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-04-07 16:40:35 -0400

    Añadida interpolación lineal banco de bateríás

--- QuasarFrontEnd/src/components/Fenix/BancoBaterias.vue
+++ QuasarFrontEnd/src/components/Fenix/BancoBaterias.vue
@@ -19,7 +19,7 @@
                       {{ bms_temp[index*2 + 1] }}
                     </div>
                   </div>
-                  <div class="text-center numero">
+                  <div class="text-center numero" :style="rgbInterpolation(bms_volt[(index)])">
                     {{ bms_volt[index] }}
                   </div>
                 </div>
@@ -42,7 +42,7 @@
                       {{ bms_temp[(index + 1)*2 + 1] }}
                     </div>
                   </div>
-                  <div class="text-center numero">
+                  <div class="text-center numero" :style="rgbInterpolation(bms_volt[(index + 1)])">
                     {{ bms_volt[(index + 1)] }}
                   </div>
                 </div>
@@ -65,7 +65,7 @@
                       {{ bms_temp[(index + 2)*2 + 1] }}
                     </div>
                   </div>
-                  <div class="text-center numero">
+                    <div class="text-center numero" :style="rgbInterpolation(bms_volt[(index + 2)])">
                     {{ bms_volt[(index + 2)] }}
                   </div>
                 </div>
@@ -81,6 +81,63 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'BancoBaterias',
+  methods: {
+    createBackgroundString (index) {
+      var a = '#000000'
+      var b = '#ffffff'
+      var maxV = 4.2
+      var minV = 3.4
+      var amount = (index - minV) / (maxV - minV)
+
+      var ah = +a.replace('#', '0x'),
+        ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
+        bh = +b.replace('#', '0x'),
+        br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
+        rr = ar + amount * (br - ar),
+        rg = ag + amount * (bg - ag),
+        rb = ab + amount * (bb - ab)
+
+      var r = '{background-color: ' + '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1) + ';}'
+      console.log(r)
+      return r
+    },
+    rgbInterpolation (valor) {
+      var maxV = 4.2
+      var minV = 3.4
+      var middle = (maxV + minV) / 2
+      // var amount = (valor - minV) / (maxV - minV)
+      var amount = 255 / (middle - minV)
+      console.log(amount)
+
+      // var color1 = [255, 0, 0]
+      // var color2 = [0, 255, 0]
+
+      var r = 0
+      var g = 0
+      var b = 0
+
+      if (valor <= minV) {
+        r = 255
+        console.log('Hola')
+      } else if (valor >= maxV) {
+        g = 255
+      } else if (valor < middle) {
+        r = 255
+        g = (valor - minV) * amount
+      } else {
+        r = 255 - (valor - middle) * amount
+        g = 255
+      }
+
+      // var r = (color2[0] - color1[0]) * amount + color1[0]
+      // var g = (color2[1] - color1[1]) * amount + color1[1]
+      // var b = (color2[2] - color1[2]) * amount + color1[2]
+
+      var str = 'background-color: rgb(' + r + ',' + g + ',' + b + ');'
+      console.log(str)
+      return str
+    }
+  },
   computed: {
     ...mapState('fenix', ['bms_volt', 'bms_temp'])
   }
--- QuasarFrontEnd/src/store/module-fenix/state.js
+++ QuasarFrontEnd/src/store/module-fenix/state.js
@@ -5,7 +5,7 @@ export default function () {
     kelly_izq: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], // 14 [ia, ib, ic, va, vb, vc, rpm, err_code, pwm, emr, motor_temp, kelly_temp, throttle, reverse]
     bms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], // 15 [SOC, current, inst_volt, open_volt, abs_current, max_volt, min_volt, max_temp, max_temp_id, min_temp, min_temp_id, avg_temp, internal_temp, max_volt_id, min_volt_id]
     bms_temp: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 60
-    bms_volt: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 30
+    bms_volt: [3.4, 3.5, 3.65, 3.75, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 30
     mppt1: [0, 1, 2, 3, 4, 5, 6, 7], // 8 [uin, iin, uout, blvr, ovt, noc, undv, temp]
     mppt2: [0, 1, 2, 3, 4, 5, 6, 7], // 8
     mppt3: [0, 1, 2, 3, 4, 5, 6, 7], // 8
