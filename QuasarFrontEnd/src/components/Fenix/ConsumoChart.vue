694bc4bc298efab47333dcb54758d06aec3bce6c
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-04-14 16:08:56 -0400

    Encontrada forma reactiva pero ineficiente en rendimiento

--- QuasarFrontEnd/src/components/Fenix/ConsumoChart.vue
+++ QuasarFrontEnd/src/components/Fenix/ConsumoChart.vue
@@ -1,6 +1,7 @@
 <script>
 import { Line } from 'vue-chartjs'
 import 'chartjs-plugin-streaming'
+import { mapState } from 'vuex'
 
 var chartColors = {
   red: 'rgb(255, 99, 132)',
@@ -12,116 +13,117 @@ var chartColors = {
   grey: 'rgb(201, 203, 207)'
 }
 
-function randomScalingFactor () {
-  return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100)
-}
+// function randomScalingFactor () {
+//   return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100)
+// }
 
-function onRefresh (chart) {
-  chart.config.data.datasets.forEach(function (dataset) {
-    dataset.data.push({
-      x: Date.now(),
-      y: randomScalingFactor()
-    })
-  })
-}
-
-var data = {
-  datasets: [{
-    label: 'Velocidad',
-    backgroundColor: chartColors.red,
-    borderColor: chartColors.red,
-    fill: false,
-    cubicInterpolationMode: 'monotone',
-    data: []
-  }, {
-    label: 'Consumo',
-    backgroundColor: chartColors.blue,
-    borderColor: chartColors.blue,
-    fill: false,
-    cubicInterpolationMode: 'monotone',
-    data: []
-  }, {
-    label: 'Paneles',
-    backgroundColor: chartColors.yellow,
-    borderColor: chartColors.yellow,
-    fill: false,
-    cubicInterpolationMode: 'monotone',
-    data: []
-  }]
-}
-
-var options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  title: {
-    display: true,
-    text: 'Consumo en Watts'
-  },
-  scales: {
-    xAxes: [{
-      type: 'realtime',
-      realtime: {
-        duration: 20000,
-        refresh: 1000,
-        delay: 2000,
-        onRefresh: onRefresh
-      }
-    }],
-    yAxes: [{
-      scaleLabel: {
-        display: true,
-        labelString: 'value'
-      }
-    }]
-  },
-  tooltips: {
-    mode: 'nearest',
-    intersect: false
-  },
-  hover: {
-    mode: 'nearest',
-    intersect: false
-  },
-  legend: {
-    position: 'bottom'
-  }
-}
+// function onRefresh1 (chart) {
+//   chart.config.data.datasets.forEach(function (dataset) {
+//     dataset.data.push({
+//       x: Date.now(),
+//       y: randomScalingFactor()
+//     })
+//   })
+// }
 
 export default {
   extends: Line,
-  mounted () {
-    this.renderChart(data, options)
+  computed: {
+    ...mapState('fenix', ['mainData'])
   },
-  methods: {
-    updateChart (data) {
-      this.renderChart({
+  watch: {
+    mainData: function (newValue, oldValue) {
+      // console.log('was: ', oldValue, ' now: ', newValue)
+      this.$data._chart.data.datasets.forEach((dataset) => {
+        dataset.data.push({
+          x: Date.now(),
+          y: newValue[0]
+        })
+      })
+      this.$data._chart.update()
+      // console.log(this.$data._chart)
+      // this.updateChart(newValue)
+    }
+  },
+  mounted () {
+    this.renderChart(
+      // Data
+      {
         datasets: [{
-          label: 'Dataset 1',
-          borderColor: 'rgb(255, 99, 132)',
-          backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          lineTension: 0,
-          borderDash: [8, 4]
+          label: 'Velocidad',
+          backgroundColor: chartColors.red,
+          borderColor: chartColors.red,
+          fill: false,
+          cubicInterpolationMode: 'monotone',
+          data: []
         }, {
-          label: 'Dataset 2',
-          borderColor: 'rgb(54, 162, 235)',
-          backgroundColor: 'rgba(54, 162, 235, 0.5)'
+          label: 'Consumo',
+          backgroundColor: chartColors.blue,
+          borderColor: chartColors.blue,
+          fill: false,
+          cubicInterpolationMode: 'monotone',
+          data: []
+        }, {
+          label: 'Paneles',
+          backgroundColor: chartColors.yellow,
+          borderColor: chartColors.yellow,
+          fill: false,
+          cubicInterpolationMode: 'monotone',
+          data: []
         }]
-      }, {
+      }
+      ,
+      // Options
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: 'Consumo en Watts'
+        },
         scales: {
           xAxes: [{
-            realtime: {
-              onRefresh: function (chart) {
-                chart.data.datasets.forEach(function (dataset) {
-                  dataset.data.push({
-                    x: Date.now(),
-                    y: Math.random()
-                  })
-                })
-              },
-              delay: 2000
+            type: 'realtime'// ,
+            // realtime: {
+            //   duration: 20000,
+            //   refresh: 1000,
+            //   delay: 2000,
+            //   onRefresh: function (chart) {
+            //     console.log('onrefreshh')
+            //     chart.config.data.datasets.forEach(function (dataset) {
+            //       dataset.data.push({
+            //         x: Date.now(),
+            //         y: 0
+            //       })
+            //     })
+            //   }
+            // }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'value'
             }
           }]
+        },
+        tooltips: {
+          mode: 'nearest',
+          intersect: false
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: false
+        },
+        legend: {
+          position: 'bottom'
         }
+      } // End Options
+    ) // End renderChart
+  }, // End Mounted
+  methods: {
+    updateChart (data) {
+      this.chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data[0])
       })
     }
   }
