1fc07ea87d8e00b2d80e3d3741658f89b2636a00
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-04-20 13:37:21 -0400

    Métodos definitivos de update

--- QuasarFrontEnd/src/store/module-fenix/mutations.js
+++ QuasarFrontEnd/src/store/module-fenix/mutations.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+/* Mutations para debug, test y pruebas */
 
 export function updateVelocidad (state) {
   Vue.set(state.mainData, 0, state.mainData[0] + 1) // state.mainData[0]++ No funciona, usar Vue.set https://vuejs.org/v2/guide/reactivity.html#Change-Detection-Caveats
@@ -41,3 +42,161 @@ export function updateTemps (state) {
   Vue.set(state.kelly_izq, 11, state.kelly_izq[11] + 3)
   Vue.set(state.data_grafico_2, 2, state.kelly_izq[11])
 }
+
+/* Mutations reales de update de arreglos de componentes en el store */
+
+export function update_mainData(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'mainData', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_kelly_der(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'kelly_der', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_kelly_izq(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'kelly_izq', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_bms(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'bms', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_bms_temp(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'bms_temp', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_bms_volt(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'bms_volt', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_mppt1(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'mppt1', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_mppt2(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'mppt2', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_mppt3(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'mppt3', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_mppt4(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'mppt4', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+export function update_potencia_mppt(state, array) {
+  // Formateamos a tres decimales para visualizar bien
+
+  var x = 0
+  var l = array.length
+  while (x < l) {
+    array[x] = Math.round(a[x] * 1000) / 1000
+    x++
+  }
+
+  // Cambiamos el state
+  Vue.set(state, 'potencia_mppt', [...array]) // [...array] Copiamos para prevenir cambios de terceros
+}
+
+/* Update de data_grafico_1 y data_grafico_2 en mutaciones que contienen esos datos (mainData, kelly_der, kelly_izq, bms) */
