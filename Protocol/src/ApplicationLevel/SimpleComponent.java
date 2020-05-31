55c274d3b53d483a00d575878d9be55f605c8f46
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-05-31 18:22:23 -0400

    Se crea SimpleComponent para capa de Aplicación. Se dividen responsabilidades entre capas.

---
+++ Protocol/src/ApplicationLevel/SimpleComponent.java
@@ -0,0 +1,34 @@
+package ApplicationLevel;
+
+import Protocol.Components.Component;
+
+/**
+ * Será una clase usada como interfaz entre un Componente del Protocolo y los datos básicos que definen una componente.
+ * Se podrán cargar datos desde un archivo de configuración a esta componente.
+ * Puede decirse que esta Componente está en la capa de aplicación.
+ * Cada SimpleComponent tiene un Component del protocolo asociado.
+ * Se les asocia un SensorReader específico que, después de escalar, hará update directo del Component del Protocolo.
+ */
+public class SimpleComponent {
+    private final String ID;
+    private final double[] minimosConDecimal;
+    private final double[] maximosConDecimal;
+
+    private Component myProtocolComponent;
+
+    /**
+     * SimpleComponent sólo se caracteriza por sus valores mínimos, máximos, y su ID que se usará para muchas cosas.
+     * Incluyendo el nombre de eventos en Socket.IO
+     * @param id Nombre del SimpleComponente
+     * @param minimosConDecimal Valores mínimos de cada valor del componente
+     * @param maximosConDecimal Valores máximos de cada valor del componente
+     */
+    public SimpleComponent(String id, double[] minimosConDecimal, double[] maximosConDecimal) {
+        this.ID = id;
+        this.minimosConDecimal = minimosConDecimal;
+        this.maximosConDecimal = maximosConDecimal;
+    }
+
+
+
+}
