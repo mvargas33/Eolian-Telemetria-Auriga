4f165aea55cabe6fd6f60df99926e44255fd21e9
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-06-15 20:41:13 -0400

    Added crypto-related test

--- Protocol/src/PresentationLayer/Encryption/CryptoAdmin.java
+++ Protocol/src/PresentationLayer/Encryption/CryptoAdmin.java
@@ -38,7 +38,7 @@ public class CryptoAdmin {
     /**
      * Base Encryptor.
      */
-    private CryptoAdmin(SecretKey key, byte[] IV, int MAC_SIG_BYTES, int IV_SIG_BYTES, int CONTENT_SIG_BYTES) throws Exception {
+    public CryptoAdmin(SecretKey key, byte[] IV, int MAC_SIG_BYTES, int IV_SIG_BYTES, int CONTENT_SIG_BYTES) throws Exception {
         if(CONTENT_SIG_BYTES % 16 != 15){
             throw new Exception("CONTENT BYTES SIZE IS NOT OPTIMAL. USE MESSAGE LIKE 16 | 16 | 16 | 15 + 1 (PADDING)");
         }
@@ -55,7 +55,7 @@ public class CryptoAdmin {
         this.IV_START = new byte[IV_FIXED_BYTES];
         this.IV_END = new byte[IV_SIG_BYTES];
         this.IV_END_ZEROS = new byte[IV_SIG_BYTES];
-        this.IV_END_INTEGER = new BigInteger(IV_END);
+
         this.IV_END_MOD = new byte[IV_SIG_BYTES + 1]; // + 1 Sign byte
 
         this.inputEncrypted = new byte[CONTENT_SIG_BYTES + 1]; // +1 PADDING BYTE
@@ -68,6 +68,7 @@ public class CryptoAdmin {
 
         System.arraycopy(IV, IV_SIG_BYTES, IV_START, 0, IV_FIXED_BYTES);
         System.arraycopy(IV, 0, IV_END, 0, IV_SIG_BYTES);
+        this.IV_END_INTEGER = new BigInteger(IV_END);
 
         byte[] zero = {(byte) 0b00000000};
         byte[] one = {(byte) 0b11111111};
@@ -88,7 +89,9 @@ public class CryptoAdmin {
         // Update IV_END array
         if(result.length == 17 - IV_FIXED_BYTES){ // Viene con bit de signo
             System.arraycopy(result, 1, IV_END, 0, IV_SIG_BYTES); // No considero bit de signo
-        }else{ // Viene con 16-4 bytes O MENOS
+        }else if (result.length == 16 - IV_FIXED_BYTES){ // Viene con 16-4 bytes O MENOS
+            System.arraycopy(result, 0, IV_END, 0, result.length); // Copiar los bytes de IV_END
+        }else{
             System.arraycopy(result, 0, IV_END, 0, result.length); // Copiar los bytes de IV_END
             System.arraycopy(IV_END_ZEROS, 0, IV_END, result.length, 16 - result.length); // Rellenar con 0s
         }
@@ -113,6 +116,8 @@ public class CryptoAdmin {
         System.arraycopy(macEnd, 0, fullMessage, 0, MAC_SIG_BYTES);
         System.arraycopy(IV_END, 0, fullMessage, MAC_SIG_BYTES, IV_SIG_BYTES);
         System.arraycopy(inputEncrypted, 0, fullMessage, MAC_SIG_BYTES + IV_SIG_BYTES, CONTENT_SIG_BYTES + 1);
+        // Change IV for a clean new encryption
+        this.addOneToIVEND();
         // Return.
         return this.fullMessage;
     }
---
+++ Protocol/src/Test/CryptoTest.java
@@ -0,0 +1,43 @@
+package Test;
+
+import ApplicationLayer.ApplicationComponent;
+import PresentationLayer.Encryption.CryptoAdmin;
+import PresentationLayer.Encryption.KeyAdmin;
+import org.junit.jupiter.api.BeforeEach;
+
+import javax.crypto.SecretKey;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CryptoTest {
+    KeyAdmin keyAdmin;
+    CryptoAdmin encryptor;
+    CryptoAdmin decryptor;
+    String plainText;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        keyAdmin = new KeyAdmin();
+        keyAdmin.genNewIV();
+        keyAdmin.genNewKey();
+
+        plainText = "012345678901234"; // 15 Bytes
+
+        int MAC_SIG_BYTES = 6;
+        int IV_SIG_BYTES = 12;
+        int CONTENT_SIG_BYTES = plainText.length();
+
+
+        encryptor = new CryptoAdmin(keyAdmin.getKey(), keyAdmin.getIV(), MAC_SIG_BYTES, IV_SIG_BYTES, CONTENT_SIG_BYTES);
+        decryptor = new CryptoAdmin(keyAdmin.getKey(), keyAdmin.getIV(), MAC_SIG_BYTES, IV_SIG_BYTES, CONTENT_SIG_BYTES);
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void simpleEncryptionDecryption() throws Exception {
+        byte[] encryptedText = encryptor.encrypt(plainText.getBytes());
+        byte[] decryptedText = decryptor.decrypt(encryptedText);
+        assertArrayEquals(plainText.getBytes(), decryptedText);
+    }
+}
