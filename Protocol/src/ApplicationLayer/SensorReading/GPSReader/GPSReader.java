ecc02fd439b2bd882016223287ad70933d827ba0
Jayki-ZX <danteantu5@gmail.com>
2020-10-14 18:51:50 -0300

    init GPSReader

---
+++ Protocol/src/ApplicationLayer/SensorReading/GPSReader/GPSReader.java
@@ -0,0 +1,12 @@
+package ApplicationLayer.SensorReading.GPSReader;
+
+import ApplicationLayer.SensorReading.SensorsReader;
+
+public class GPSReader implements SensorsReader {
+
+
+    @Override
+    public double[] read() {
+        return new double[0];
+    }
+}
