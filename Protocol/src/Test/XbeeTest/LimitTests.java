c2705c453346d356d15cbe2dda4ad07446f43bb2
Maximiliano Vargas <maximiliano.vargas@ug.uchile.cl>
2020-11-27 19:41:22 -0300

    Test: Made Xbee Tests. Reorganized Tests

---
+++ Protocol/src/Test/XbeeTest/LimitTests.java
@@ -0,0 +1,11 @@
+package Test.XbeeTest;
+
+public class LimitTests {
+
+    @org.junit.jupiter.api.Test
+    public void messageLimitSizeTest(){
+        // Mediante fuerza bruta, envía mensajes cada vez más largos hasta que se arroja un error.
+        // Determina el largo máx en bytes de mensajes de las Xbee
+
+    }
+}
